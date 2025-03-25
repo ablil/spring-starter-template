e0d9822bf89fc62e8c26f43336e865e88c04ba29
ablil <ablil@proton.me>
2025-03-25 17:08:56 +0000

    configure spotless

--- src/main/kotlin/com/example/SpringStarterTemplateApplication.kt
+++ src/main/kotlin/com/example/SpringStarterTemplateApplication.kt
@@ -3,9 +3,8 @@ package com.example
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class SpringStarterTemplateApplication
+@SpringBootApplication class SpringStarterTemplateApplication
 
 fun main(args: Array<String>) {
-	runApplication<SpringStarterTemplateApplication>(*args)
+    runApplication<SpringStarterTemplateApplication>(*args)
 }
