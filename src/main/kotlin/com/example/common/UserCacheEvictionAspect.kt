e63331654991c1681f28a1e10014d7f92f8f0037
ablil <ablil@proton.me>
2025-04-02 19:22:04 +0000

    feat: cache user entity when validating jwt token

---
+++ src/main/kotlin/com/example/common/UserCacheEvictionAspect.kt
@@ -0,0 +1,31 @@
+package com.example.common
+
+import com.example.users.DomainUser
+import com.example.users.USERS_BY_LOGIN
+import com.example.users.getLogger
+import org.aspectj.lang.annotation.AfterReturning
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Pointcut
+import org.springframework.cache.CacheManager
+import org.springframework.stereotype.Component
+
+@Aspect
+@Component
+class UserCacheEvictionAspect(val cacheManager: CacheManager) {
+
+    val logger = getLogger()
+
+    @Pointcut("execution(* com.example.*.UserRepository*.saveAndFlush(..))")
+    fun anyUserPersistence() {
+        // do nothing
+    }
+
+    @AfterReturning("anyUserPersistence()", returning = "retValue")
+    fun clearCache(retValue: Any) {
+        if (retValue !is DomainUser) {
+            logger.warn("advice called with invalid return value {}", retValue)
+            return
+        }
+        cacheManager.getCache(USERS_BY_LOGIN)?.evictIfPresent(retValue.username)
+    }
+}
