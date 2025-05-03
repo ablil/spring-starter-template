d3d256b345759773e4ba11c0849585af8d7260a2
ablil <ablil@proton.me>
2025-05-03 15:44:18 +0100

    build: switch gradle scripts from Groovy to Kotlin

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+rootProject.name = "spring-starter-template"
