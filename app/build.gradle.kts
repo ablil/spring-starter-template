a3cfce205d8a84d984cfee8ea90496816e108a20
ablil <ablil@proton.me>
2025-05-10 19:54:27 +0100

    refactor: restructure project into multi-module

---
+++ app/build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.23"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.9.23"
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management")
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.23"
+    id("com.diffplug.spotless") version "7.0.3"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    id("com.google.cloud.tools.jib") version "3.4.5"
+}
+
+
+val isCI = System.getenv("CI") == "true"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.apache.commons:commons-lang3:3.17.0")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.springframework.security:spring-security-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("com.h2database:h2")
+    detektPlugins("com.github.ablil:detekt-extension:v0.1.0")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+spotless {
+    kotlin {
+        toggleOffOn()
+        ktfmt("0.54").kotlinlangStyle()
+    }
+}
+
+tasks.build {
+    if (isCI) {
+        dependsOn(tasks.jib)
+    }
+}
+
+jib {
+    to {
+        val imageTag = project.findProperty("tag")
+        image = "ghcr.io/ablil/spring-starter-template:$imageTag".takeIf { isCI } ?: "spring-starter-template:$imageTag"
+    }
+    container {
+        creationTime = "USE_CURRENT_TIMESTAMP"
+        ports = listOf("8080")
+    }
+}
+
+allOpen {
+    annotation("jakarta.persistence.Entity")
+    annotation("jakarta.persistence.MappedSuperclass")
+    annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.test {
+    systemProperty("spring.profiles.active", "test")
+    useJUnitPlatform()
+}
+
+springBoot {
+    buildInfo()
+}
+
+detekt {
+    config.setFrom("src/main/resources/config/config.yml")
+    buildUponDefaultConfig = true
+}
