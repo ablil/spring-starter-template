625eed7d27c28c4d32b0a04e0beeb636f277cdae
ablil <ablil@proton.me>
2025-05-17 15:46:05 +0100

    feat: include git properties on actuator info

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.23"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.gorylenko.gradle-git-properties") version "2.5.0"
 }
 
 group = "com.example"
