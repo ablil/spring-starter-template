1ccf4434d6cf014cfde2ead321915ecf9e13e9ad
ablil <ablil@proton.me>
2025-06-28 11:44:08 +0100

    refactor: rename main class and extract global configuration

--- app/src/test/kotlin/com/example/MainApplicationTests.kt
+++ app/src/test/kotlin/com/example/MainApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringStarterTemplateApplicationTests {
+class MainApplicationTests {
 
     @Suppress("EmptyFunctionBlock") @Test fun `should load context`() {}
 }
