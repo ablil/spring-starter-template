63e0a99a41d8699da603d224844f3c8d83dbf3db
ablil <ablil@proton.me>
2025-06-28 11:47:39 +0100

    feat: define application exception

--- app/src/main/kotlin/com/example/common/ExceptionHandler.kt
+++ app/src/main/kotlin/com/example/common/ExceptionHandler.kt
@@ -6,7 +6,10 @@ import org.springframework.http.ProblemDetail
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) class ApplicationException : RuntimeException()
 
 @RestControllerAdvice
 class ExceptionHandler {
