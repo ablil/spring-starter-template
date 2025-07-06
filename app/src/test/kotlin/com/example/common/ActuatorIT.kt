d0afcb6205917c5090e436308b220e5e1200de58
ablil <ablil@proton.me>
2025-07-06 14:58:01 +0100

    fix: update hibernate properties

--- app/src/test/kotlin/com/example/common/ActuatorIT.kt
+++ app/src/test/kotlin/com/example/common/ActuatorIT.kt
@@ -1,11 +1,9 @@
 package com.example.common
 
-import com.example.users.WithMockAdmin
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 
@@ -18,26 +16,5 @@ class ActuatorIT {
     @Test
     fun `should access health endpoint given no credentials`() {
         mockMvc.get("/actuator/health").andExpect { status { isOk() } }
-    }
-
-    @Test
-    @WithMockAdmin
-    fun `should access info endpoint given an admin`() {
-        mockMvc.get("/actuator/info").andExpectAll {
-            status { isOk() }
-            jsonPath("$.build.time") { exists() }
-        }
-    }
-
-    @Test
-    @WithMockAdmin
-    fun `should access loggers endpoint given an admin user`() {
-        mockMvc.get("/actuator/loggers").andExpect { status { isOk() } }
-    }
-
-    @Test
-    @WithMockUser
-    fun `should not access loggers endpoint given non-admin users`() {
-        mockMvc.get("/actuator/loggers").andExpect { status { isForbidden() } }
     }
 }
