bde32005e650d033424d1b3e28fd6a66475c728b
ablil <ablil@proton.me>
2025-11-02 11:19:00 +0100

    feat: add users populator for local development

--- application/src/main/kotlin/com/example/persistence/JPAConfiguration.kt
+++ application/src/main/kotlin/com/example/persistence/JPAConfiguration.kt
@@ -3,8 +3,11 @@ package com.example.persistence
 import java.util.Optional
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.core.io.ClassPathResource
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.repository.init.Jackson2RepositoryPopulatorFactoryBean
 import org.springframework.security.core.context.SecurityContextHolder
 
 @Configuration
@@ -17,5 +20,13 @@ class JPAConfiguration {
             Optional.ofNullable(
                 SecurityContextHolder.getContext().authentication.principal?.toString()
             )
+        }
+
+    @Profile("!prod")
+    @Bean
+    fun testUsersPopulator(): Jackson2RepositoryPopulatorFactoryBean =
+        with(Jackson2RepositoryPopulatorFactoryBean()) {
+            this.setResources(arrayOf(ClassPathResource("testdata/users.json")))
+            this
         }
 }
